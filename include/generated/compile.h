95be4f8566201f6525db7a4550f4fb25274cc072
lisongze <Songze_Lee@163.com>
2016-12-31 13:31:21 +0800

    add tiny4412 linux-3.0.86 kernel for andriod 5.0.2 by lisongze

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 31 12:20:55 CST 2016"
+#define LINUX_COMPILE_BY "lisongze"
+#define LINUX_COMPILE_HOST "lisongze"
+#define LINUX_COMPILER "gcc version 4.5.1 (ctng-1.8.1-FA) "
